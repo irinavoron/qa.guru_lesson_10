0c2f1cadfd1e5fde88f3766e97bc6dc85bf880c2
Irina.Voron <yarutich.irina@gmail.com>
2024-03-13 12:36:23 +0300

    Delete tests

--- tests/test_attachments.py
+++
@@ -1,8 +0,0 @@
-import json
-import allure
-from allure import attachment_type
-
-def test_attachments():
-    allure.attach('Text content', 'Text', attachment_type=attachment_type.TEXT)
-    allure.attach('T<h1> hello world! </h1>', 'HTML', attachment_type=attachment_type.HTML)
-    allure.attach(json.dumps({'First': 1, 'Second': 2}), 'JSON', attachment_type=attachment_type.JSON)
